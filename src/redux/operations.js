c160896b8f50020ef0a98c602cf90d7b567a2d3f
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-12-13 15:33:10 +0200

    update base url

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://63888685a4bb27a7f789b027.mockapi.io';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
