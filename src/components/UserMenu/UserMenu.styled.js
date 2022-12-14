a955c38b820faca9a7433b722efdcf763095b0ab
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-12-14 14:06:05 +0200

    fix logout button styles

--- src/components/UserMenu/UserMenu.styled.js
+++ src/components/UserMenu/UserMenu.styled.js
@@ -12,20 +12,31 @@ export const UserName = styled.p`
 
 export const Button = styled.button`
   display: block;
-  background-color: ${p => p.theme.colors.btnBg};
-  color: ${p => p.theme.colors.white};
-  font-weight: ${p => p.theme.fontWeight.bold};
-  border-radius: ${p => p.theme.radii.normal};
-  padding-top: ${p => p.theme.space[2]}px;
-  padding-bottom: ${p => p.theme.space[2]}px;
-  border: none;
+
+  min-width: 150px;
+  height: 40px;
+  border: 0px solid transparent;
+  border-radius: 4px;
+
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 5px;
+  margin-bottom: 5px;
+
+  color: #fff;
+  background-color: #2196f3;
+
+  box-shadow: 0px 4px 4px rgb(0 0 0 / 15%);
+
+  opacity: 1;
+
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
   cursor: pointer;
-  transition: all 250ms cubic-bezier(0.075, 0.82, 0.165, 1);
 
   :hover,
   :focus {
-    background-color: ${p => p.theme.colors.btnHover};
-    box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.75);
-    color: ${p => p.theme.colors.black};
+    opacity: 0.8;
+    transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
   }
 `;
