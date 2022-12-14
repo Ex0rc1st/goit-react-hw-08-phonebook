2360c27ec3952d94800f80aa1be406a60971f682
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-12-14 14:10:06 +0200

    text fixed

--- src/components/PhoneBook/PhoneBook.js
+++ src/components/PhoneBook/PhoneBook.js
@@ -50,7 +50,7 @@ export default function PhoneBook() {
           {isLoading && !error ? (
             <FadeLoader color={'#337ab7'} />
           ) : (
-            <Notice>PhoneBook book is empty</Notice>
+            <Notice>PhoneBook is empty</Notice>
           )}
         </Box>
       )}
